ea94cfee2f8b5bbc7f3e942beb2eb8ad7f61f318
giorgideveloper <gorgiladze888@gmail.com>
2024-12-23 14:03:56 +0400

    `feat: updated dependencies and added async storage`

---
+++ app/startScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const StartScreen = () => {
+	return (
+		<View>
+			<Text>Start Screen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default StartScreen;
